047087e148f2155f046afd9fa546e148dd89188a
PIROTAIS Loan <pirotais.loan@gmail.com>
2020-10-06 11:32:58 +0200

    Ajout de l'assertion unique sur le pseudo d'un participant

--- src/Entity/Participant.php
+++ src/Entity/Participant.php
@@ -6,6 +6,7 @@ use App\Repository\ParticipantRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ParticipantRepository::class)
@@ -21,6 +22,7 @@ class Participant
 
     /**
      * @ORM\Column(type="string", length=30)
+     * @Assert\Unique(message="Le pseudo est déjà utilisé")
      */
     private $pseudo;
 
