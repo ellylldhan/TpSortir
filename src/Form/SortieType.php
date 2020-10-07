e18719792fe6fee9b289e3d841a1e4fca3fc90d3
eperia35 <thomas.huet@hotmail.fr>
2020-10-07 12:09:16 +0200

    ajout du formulaire

---
+++ src/Form/SortieType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sortie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('dateDebut')
+            ->add('duree')
+            ->add('dateCloture')
+            ->add('nombreInscriptionsMax')
+            ->add('descriptionInfo')
+            ->add('etatSortie')
+            ->add('urlPhoto')
+            ->add('organisateur')
+            ->add('lieu')
+            ->add('etat')
+            ->add('campusOrganisateur')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
