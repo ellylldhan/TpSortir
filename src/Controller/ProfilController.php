52b2accf8c06862a6a353bb9472af502d93579e4
Guillaume Sabin <pudou@AirdeGuillaume.home>
2020-10-14 18:07:10 +0200

    Controle sur l'edition du profil & injection en BDD

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -7,6 +7,7 @@ use App\Form\ParticipantType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ProfilController extends AbstractController
 {
@@ -35,49 +36,77 @@ class ProfilController extends AbstractController
      * @param int $id
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function editProfile(int $id, Request $request){
+    public function editProfile(int $id, Request $request, UserPasswordEncoderInterface $encoder){
 
         // Récupération du profil
-        $user = $this->findUser($id); //$this->getUser();
-        dump($user);
+        $userConnected = $this->getUser();
+        $userToUpdate = $this->findUser($id);
 
         // L'utilisateur n'éxiste pas
-        if(!$user) $this->notFound();
+        if(!$userToUpdate) $this->notFound();
+
+        // l'utilisateur courant est-il le même que celui qu'on cherche à modifier
+        if($userConnected->getId() != $userToUpdate->getId()) {
+            throw $this->createAccessDeniedException("Vous ne pouvez pas modifier le profil d'un autre participant.");
+        }
+
+        $manager = $this->getDoctrine()->getManager();
 
         // Création du formulaire
-        $form = $this->createForm(ParticipantType::class, $user);
+        $form = $this->createForm(ParticipantType::class, $userConnected);
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
-            $avatarFile = $form->get('urlPhoto')->getData();
-            $user = $form->getData();
-            dump($user);
-            if($avatarFile){
-                $originalFilename = pathinfo($avatarFile->getClientOriginalName(), PATHINFO_FILENAME);
-                // this is needed to safely include the file name as part of the URL
-                $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
-                $newFilename = $safeFilename.'-'.uniqid().'.'.$avatarFile->guessExtension();
 
+            // On récupère l'entité depuis le formulaire
+            $userConnected = $form->getData();
+
+            // Le pseudo existe-t-il déjà en base
+            $pseudo = $form->get('pseudo')->getData();
+            if($manager->getRepository(Participant::class)->findOneBy(['pseudo' => $pseudo])){
+                $this->addFlash("danger", "Ce pseudo éxiste déjà");
+            }
+
+            // On récupère la photo
+            $avatar = $form->get('urlPhoto')->getData();
+
+            // Géstion de l'image utilisateur
+            if($avatar){
+
+                $originalFilename = pathinfo($avatar->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$avatar->guessExtension();
                 try {
-                    $avatarFile->move(
+                    $avatar->move(
                         $this->getParameter('avatars_directory'),
                         $newFilename
                     );
                 } catch (FileException $e) {
-
+                    $this->addFlash("danger", "Une erreur est survenue");
+                    return $this->render('profil/profil-edit.html.twig', [
+                        'controller_name' => 'ProfilController',
+                        'form' => $form->createView(),
+                        'title' => 'Edition de mon profil'
+                    ]);
                 }
-
-                $user->setUrlPhoto(new File($this->getParameter('avatars_directory').'/'.$user->getBrochureFilename()));
-
-                $manager = $this->getDoctrine()->getManager();
-                $manager->persist($user);
-                $manager->flush($user);
-                $user = $manager->find($user->getId());
-
-                return $this->redirectToRoute('edit-profil', [
-                    'user' => $user
-                ]);
+                $userConnected->setUrlPhoto($newFilename);
             }
+
+            // Géstion du mot de passe
+            $hashed = $encoder->encodePassword($userConnected, $userConnected->getPassword());
+            $userConnected->setPassword($hashed);
+
+            // On persiste le nouvel état de l'entité
+            $manager->persist($userConnected);
+            $manager->flush();
+            $userConnected = $this->findUser($userConnected->getId());
+
+            $this->addFlash("success", "Votre profil à bien été mis à jour");
+
+            return $this->redirectToRoute('profil', [
+                'user' => $userConnected,
+                'id' => $userConnected->getId()
+            ]);
         }
 
         return $this->render('profil/profil-edit.html.twig', [
